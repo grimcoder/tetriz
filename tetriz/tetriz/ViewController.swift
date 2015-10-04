93316a18eae1288b9e60730333185fc36367c0bf
grimcoder <papochka12>
2015-10-03 23:57:11 -0700

    initial commit of the glorious game tetris

---
+++ tetriz/tetriz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tetriz
+//
+//  Created by Taras Kovtun on 10/3/15.
+//  Copyright © 2015 Taras Kovtun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
